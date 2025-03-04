5c96a58b4458d023318f10ed3d8ae742bbae735a
cuong.phan2 <cuong.phan2@mservice.com.vn>
2025-03-04 14:01:14 +0700

    Fix: status bar for ios

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,30 +1,23 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import { CustomPaletteWrapper } from "@/components/common/custom-palette-wrapper";
+import { ToastRoot } from "@/components/common/toast";
+import { LocaleProvider } from "@/locales/provider";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { PortalHost } from "@rn-primitives/portal";
 import { useFonts } from "expo-font";
+import { LinearGradient } from "expo-linear-gradient";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
-import "react-native-reanimated";
-import { PostHogProvider } from "posthog-react-native";
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { LocaleProvider } from "@/locales/provider";
-import { cssInterop } from "nativewind";
-import { LinearGradient } from "expo-linear-gradient";
-import Svg from "react-native-svg";
 import LottieView from "lottie-react-native";
-
-import "../global.css";
-import { CustomPaletteWrapper } from "@/components/common/custom-palette-wrapper";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { cssInterop } from "nativewind";
+import { PostHogProvider } from "posthog-react-native";
+import { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
-import { ToastRoot } from "@/components/common/toast";
-import { PortalHost } from "@rn-primitives/portal";
 import { KeyboardProvider } from "react-native-keyboard-controller";
+import "react-native-reanimated";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Svg from "react-native-svg";
+import "../global.css";
 import "../utils/ReactotronConfig";
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -32,23 +25,23 @@ SplashScreen.preventAutoHideAsync();
 cssInterop(Svg, {
   className: {
     target: "style",
-    nativeStyleToProp: { width: true, height: true },
-  },
+    nativeStyleToProp: { width: true, height: true }
+  }
 });
 cssInterop(LinearGradient, {
   className: {
-    target: "style",
-  },
+    target: "style"
+  }
 });
 
 cssInterop(LottieView, {
   className: {
-    target: "style",
-  },
+    target: "style"
+  }
 });
 
 export const unstable_settings = {
-  initialRouteName: "(app)",
+  initialRouteName: "(app)"
 };
 
 export default function RootLayout() {
@@ -56,7 +49,7 @@ export default function RootLayout() {
     "NeueMontreal-Regular": require("../assets/fonts/ppneuemontreal-book.otf"),
     "NeueMontreal-Medium": require("../assets/fonts/ppneuemontreal-medium.otf"),
     "NeueMontreal-SemiBold": require("../assets/fonts/ppneuemontreal-semibolditalic.otf"),
-    "NeueMontreal-Bold": require("../assets/fonts/ppneuemontreal-bold.otf"),
+    "NeueMontreal-Bold": require("../assets/fonts/ppneuemontreal-bold.otf")
   });
 
   useEffect(() => {
@@ -74,11 +67,11 @@ export default function RootLayout() {
       apiKey={process.env.EXPO_PUBLIC_POSTHOG_API_KEY!}
       options={{
         host: process.env.EXPO_PUBLIC_POSTHOG_HOST!,
-        disabled: false,
+        disabled: false
       }}
     >
       <LocaleProvider>
-        <ThemeProvider value={DarkTheme}>
+        <ThemeProvider value={DefaultTheme}>
           <CustomPaletteWrapper>
             <SafeAreaProvider>
               <GestureHandlerRootView>
@@ -88,18 +81,13 @@ export default function RootLayout() {
                       <Stack.Screen
                         name="(aux)"
                         options={{
-                          presentation: "modal",
+                          presentation: "modal"
                         }}
                       />
                       <Stack.Screen name="(request_card)" />
                     </Stack>
                     <ToastRoot />
                     <PortalHost />
-                    <StatusBar
-                      translucent={false}
-                      style="dark"
-                      backgroundColor="transparent"
-                    />
                   </BottomSheetModalProvider>
                 </KeyboardProvider>
               </GestureHandlerRootView>
