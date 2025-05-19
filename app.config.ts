24c0e79a88a2aaba336eab0e6b08ee3392c2975e
cuong.phan2 <cuong.phan2@mservice.com.vn>
2025-05-19 10:52:50 +0700

    feat: update expo install

--- app.config.ts
+++ app.config.ts
@@ -70,7 +70,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
       output: "static",
       favicon: "./assets/images/favicon.png"
     },
-    plugins: ["expo-router"],
+    plugins: ["expo-router", "expo-localization"],
     experiments: {
       typedRoutes: true
     },
